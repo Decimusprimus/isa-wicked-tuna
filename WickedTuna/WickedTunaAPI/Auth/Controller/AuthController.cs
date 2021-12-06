9699d81203c590bdce5606379f2c4d924d3db988
Marko <marko.ppekez25@gmail.com>
2021-12-06 17:05:16 +0100

    Added generic repositroy

---
+++ WickedTuna/WickedTunaAPI/Auth/Controller/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WickedTunaAPI.Auth.Controller
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
---
+++ WickedTuna/WickedTunaAPI/util/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WickedTunaInfrastructure;
+
+namespace WickedTunaAPI.util
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private WickedTunaDbContext _context = null;
+        private DbSet<T> _collection = null;
+
+        public Repository(WickedTunaDbContext context)
+        {
+            _context = context;
+            _collection = _context.Set<T>();
+        }
+
+        public void Delete(object id)
+        {
+            T existing = _collection.Find(id);
+            _collection.Remove(existing);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _collection.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return _collection.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            _collection.Add(obj);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            //TODO: Check later
+            _collection.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+    }
+}
